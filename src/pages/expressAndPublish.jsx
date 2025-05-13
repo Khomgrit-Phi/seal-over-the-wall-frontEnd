55f23447640dfce317e40369805e75ac0c7d55ec
Khomgrit_Ton <khomgrit.ph@gmail.com>
2025-05-13 16:12:47 +0700

    Rename ExpressAndPublish

--- src/pages/expressAndPublish.jsx
+++ src/pages/expressAndPublish.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import NextStepButton from "../components/createDesignPage/NextStepButton";
-import BackButton from "../components/collectDetailsPage/BackButton";
+import NextStepButton from "../components/createDesignPage/NextStepButton.jsx";
+import BackButton from "../components/collectDetailsPage/BackButton.jsx";
 import shirtProvWhiteFront from "../../src/assets/images/Products/shirt/prove/prove-shirt-white-front.png";
 // import bagWhiteFront from "../../src/assets/images/Products/bag/prove/prove-bag-white-front.png";
 // import cupWhiteFront from "../../src/assets/images/Products/cup/prove/prove-cup-white-front.png";
