b843d32742f786b663d0b942b05e12ad487c2ae2
z3tz3r0 <kittipan.wang@gmail.com>
2025-05-14 21:51:33 +0700

    style/feat(cart/product): add cart func and style reviews

--- src/components/productPage/ProductCard.jsx
+++ src/components/productPage/ProductCard.jsx
@@ -1,20 +1,29 @@
 import { useAuth } from "@/context/AuthContext";
+import { useState } from "react";
 import { Link } from "react-router";
 import AddToCart from "./AddToCart";
 import FavoriteMenu from "./FavoriteMenu";
 
 const ProductCard = ({ item }) => {
   const { addItemToCart } = useAuth();
+  const [randomIndex] = useState(Math.floor(Math.random() * (item.images?.length || 1)));
+  const minRating = 3;
+  const maxRating = 5;
+  const ratingSteps = (maxRating - minRating) / 0.5 + 1;
+  const [randomRating] = useState(() => {
+    const randomStep = Math.floor(Math.random() * ratingSteps);
+    return minRating + randomStep * 0.5;
+  });
+  const [randomReviewCount] = useState(Math.floor(Math.random() * 400));
 
   if (!item) {
     return null;
   }
-  const random = Math.floor(Math.random() * item.images?.length)
   return (
     <Link to={`/shop/${item._id}`} className="relative w-[248px] h-auto bg-white hover:shadow-lg hover:scale-105 hover:transition duration-300 cursor-pointer">
       <div className="relative overflow-hidden ">
         <img
-          src={`/${item.images?.[random]}`}
+          src={`/${item.images?.[randomIndex]}`}
           alt={item.title}
           className="w-full h-auto object-cover"
         />
@@ -32,22 +41,19 @@ const ProductCard = ({ item }) => {
             {[...Array(5)].map((_, i) => (
               <i
                 key={i}
-                className={`${i + 1 <= Math.floor(item.reviews || 0)
+                className={`${i + 1 <= Math.floor(randomRating)
                   ? "fa-solid fa-star text-[#334DD8]"
-                  : i < (item.reviews || 0)
+                  : i < (randomRating || 0)
                     ? "fa-solid fa-star-half-stroke text-[#334DD8]"
                     : "fa-regular fa-star text-gray-300"
                   }`}
               ></i>
             ))}
-            {/* <span className="text-sm text-gray-600 ml-2">
-                  {item.reviews} / 5
-                </span> */}
           </div>
-          <p className="">( {item.reviewCount || 0} )</p>
+          <p className="">( {randomReviewCount} )</p>
         </div>
 
-        <AddToCart item={item} addItemToCart={addItemToCart} random={random} />
+        <AddToCart item={item} addItemToCart={addItemToCart} random={randomIndex} />
       </div>
     </Link>
   );
